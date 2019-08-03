5525eb25c39c415f3ea1b3ed1cf5d1ffc3c90959
way2java <way2java@gmail.com>
2019-08-03 10:37:40 +0900

    Redux 적용 전

---
+++ src/redux/reduces/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux";
+import {playerRecuder} from "./player";
+
+export const allReducers = combineReducers({
+	// playerReducers : playerRecuder
+	playerRecuder
+	}
+)
---
+++ src/redux/reduces/player.js
@@ -0,0 +1,10 @@
+// es6 기본값 설정 추가됩
+export const playerRecuder = (state = '', action) => {
+	switch (action.type) {
+		case '' :
+			break;
+
+		default :
+			return state;
+	}
+}
